414d6918df1c1a3788276518cbabed12ed5bc9da
davidpypysp <davidpy1992@gmail.com>
2017-05-08 12:11:36 -0700

    Add project files.

---
+++ algo-practice-cpp/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "cc150/c1_5.h"
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+
+
+	char str[] = "abc def a ";
+	Replace20(str, 10);
+
+
+
+	cout << "result: " << str << endl;
+	return 0;
+}
